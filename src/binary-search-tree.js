f54d7979335824c6285ecdd108753d4430dba43b
Syderi <syderi@yahoo.com>
2022-08-08 15:11:00 +0300

    feat: add test

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,59 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
 
+  // class Node {
+  //   constructor(data) {
+  //     this.data = data;
+  //     this.left = null;
+  //     this.right = null;
+  //   }
+  // }
+  
+  // module.exports = {
+  //   Node
+  // };
+
+  constructor(data)
+    {
+        this.tree = null
+      }
+
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+  
+    let newNode = new Node(data);
+    if (this.tree === null) {
+        this.tree = newNode;
+    } else {
+        this.insertNode(this.tree, newNode); // helper method below
+    }
+}
+    insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+        if (node.left === null) {
+            node.left = newNode;
+        } else {
+            this.insertNode(node.left, newNode);
+        }
+    } else {
+        if (node.right === null) {
+            node.right = newNode;
+        } else {
+            this.insertNode(node.right, newNode);
+        }
+    }
   }
 
   has(/* data */) {
@@ -39,8 +77,13 @@ class BinarySearchTree {
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (node.right === null) {
+      return node.data
+    } else {
+      return this._findMaxNode(node.right)
+    }
   }
 }
 
