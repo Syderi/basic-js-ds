44e62e0c597f70298ff8fe0981fb5cc0a0c22d69
Syderi <syderi@yahoo.com>
2022-08-10 08:24:15 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.stack = [];
+    // this.length = 0;
+  }
+
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.stack.push(element);
+    // console.log(this.stack)
+    // this.length++;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+     return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let len = this.stack.length - 1;
+    return this.stack[len];
   }
 }
 
